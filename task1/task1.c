fd9aacea4ff961d7007020710e1236916f5ad225
Vladimir Miloserdov <milosvova@gmail.com>
2015-10-08 22:46:08 +0300

    Now all required functions are ready

--- task1/task1.c
+++ task1/task1.c
@@ -6,6 +6,12 @@
 
 #define TEST_COUNT 100
 
+// Returns 1 on Little Endian arch. and 0 on Big Endian one
+int endian_check() {
+	int bytes = 1;
+	return 0x0000000f & bytes; 
+}
+
 // Bitwise AND without &
 int bitAnd(int a, int b) {
 	return ~(~a | ~b);
@@ -50,10 +56,39 @@ int isPowerOf2(int x) {
 	return x && !(x & (x - 1));
 }
 
+// returns 1 if adding b to a will not lead to overflow of int
+// (no autotesting for this)
+int addOk(int a, int b) {
+	int s = a + b;
+	int a_sign = a >> 31, 
+	    b_sign = b >> 31,
+	    s_sign = s >> 31;
+	return !((a_sign ^ s_sign) & (b_sign ^ s_sign));
+}
+
+// returns 1 if (!x) or 0 otherwise
+int bang(int x) {
+	int t = (x | (~x + 1)) >> 31;
+	return t + 1;
+}
+
+// performs bitshift on x (math. shift)
+// (no autotesting for this)
+int logicalShift(int x, int n) {
+	return ((x >> n) & ((1 << (~n + 33)) + ~0));
+}
+
+// as (x ? y : z)
+int conditional(int x, int y, int z) {
+	int t = (x | (~x + 1)) >> 31;
+	int r = (t & y) | (~t & z);
+	return r;
+}
+
 int main() {
 	srand(time(NULL));
 	int op1, op2, i;
-	// currenly it does not check last 2 functions
+	
 	for (i = 0; i < TEST_COUNT; i++) {
 		op1 = rand(), op2 = rand();
 	
@@ -62,13 +97,23 @@ int main() {
 		assert(bitXor(op1, op2) == (op1 ^ op2));
 		assert(sign(op1) == (op1 < 0 ? -1 : (op1 == 0 ? 0: 1)));
 
-		assert(thirdBits() == 0b00100100100100100100100100100100);
 		assert(fitBits(op1, op2 % 31) == (abs(op1) <= (int) pow(2, op2 % 31)));
 		assert(getByte(op1, op2 % 4) == ((unsigned char *)(&op1))[op2 % 4]);
 		
 		assert(isPowerOf2(pow(2, op1 % 31)));
 		assert(!isPowerOf2(pow(2, op1 % 31) - op2 % 100000 - 1));
+		
+		assert(!op1 == bang(op1));
+		assert((op1 ? op1 : op2) == conditional(op1, op1, op2));
 	}
+
+	assert(thirdBits() == 0b00100100100100100100100100100100);
+	
+	// checking endian_check() function
+	unsigned int x = 0x76543210;
+	char *c = (char*) &x;
+	assert((*c == 0x10) == endian_check());
+	
 	printf("%d iterations OK\nAll tests have been passed\n", TEST_COUNT); 
 	return 0;
 }

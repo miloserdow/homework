273fc27fa7e5c576bfa2400e8252c56e0a13d02d
Vladimir Miloserdov <milosvova@gmail.com>
2015-10-09 11:50:31 +0300

    Fixed code style (func. names)

--- task1/task1.c
+++ task1/task1.c
@@ -13,23 +13,23 @@ int endian_check() {
 }
 
 // Bitwise AND without &
-int bitAnd(int a, int b) {
+int bit_and(int a, int b) {
 	return ~(~a | ~b);
 }
 
 // Bitwise OR without |
-int bitOr(int a, int b) {
+int bit_or(int a, int b) {
 	return ~(~a & ~b);
 }
 
 // Bitwise XOR using only ~ &
-int bitXor(int a, int b) {
+int bit_xor(int a, int b) {
 	int res = ~(~(a & ~b) & ~(~a & b));
 	return res;
 }
 
 // Gets a magic sequence ...100100
-int thirdBits() {
+int third_bits() {
 	return (36 << 24) | (146 << 16) | (73 << 8) | 36;
 }
 
@@ -40,25 +40,25 @@ int sign(int x) {
 
 // Returns 1 if x fits within n bits or some 0 otherwise
 // (yet it works only with positive numbers)
-int fitBits(int x, int n) {
+int fit_bits(int x, int n) {
 	return !(x >> n);
 }
 
 // Gets n-th byte of x
-int getByte(int x, int n) {
+int get_byte(int x, int n) {
 	// shift => 8 * n
 	int shift = (n << 3);
 	return (x >> shift) & 255;
 }
 
 // Returns 1 if x is a power of 2 and 0 otherwise
-int isPowerOf2(int x) {
+int is_power2(int x) {
 	return x && !(x & (x - 1));
 }
 
 // returns 1 if adding b to a will not lead to overflow of int
 // (no autotesting for this)
-int addOk(int a, int b) {
+int add_ok(int a, int b) {
 	int s = a + b;
 	int a_sign = a >> 31, 
 	    b_sign = b >> 31,
@@ -74,7 +74,7 @@ int bang(int x) {
 
 // performs bitshift on x (math. shift)
 // (no autotesting for this)
-int logicalShift(int x, int n) {
+int logical_shift(int x, int n) {
 	return ((x >> n) & ((1 << (~n + 33)) + ~0));
 }
 
@@ -92,22 +92,22 @@ int main() {
 	for (i = 0; i < TEST_COUNT; i++) {
 		op1 = rand(), op2 = rand();
 	
-		assert(bitAnd(op1, op2) == (op1 & op2));
-		assert(bitOr(op1, op2) == (op1 | op2));
-		assert(bitXor(op1, op2) == (op1 ^ op2));
+		assert(bit_and(op1, op2) == (op1 & op2));
+		assert(bit_or(op1, op2) == (op1 | op2));
+		assert(bit_xor(op1, op2) == (op1 ^ op2));
 		assert(sign(op1) == (op1 < 0 ? -1 : (op1 == 0 ? 0: 1)));
 
-		assert(fitBits(op1, op2 % 31) == (abs(op1) <= (int) pow(2, op2 % 31)));
-		assert(getByte(op1, op2 % 4) == ((unsigned char *)(&op1))[op2 % 4]);
+		assert(fit_bits(op1, op2 % 31) == (abs(op1) <= (int) pow(2, op2 % 31)));
+		assert(get_byte(op1, op2 % 4) == ((unsigned char *)(&op1))[op2 % 4]);
 		
-		assert(isPowerOf2(pow(2, op1 % 31)));
-		assert(!isPowerOf2(pow(2, op1 % 31) - op2 % 100000 - 1));
+		assert(is_power2(pow(2, op1 % 31)));
+		assert(!is_power2(pow(2, op1 % 31) - op2 % 100000 - 1));
 		
 		assert(!op1 == bang(op1));
 		assert((op1 ? op1 : op2) == conditional(op1, op1, op2));
 	}
 
-	assert(thirdBits() == 0b00100100100100100100100100100100);
+	assert(third_bits() == 0b00100100100100100100100100100100);
 	
 	// checking endian_check() function
 	unsigned int x = 0x76543210;

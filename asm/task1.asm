c6f8b60109f9d0c0c42cb8cfd18c6a71693562cb
Vladimir Miloserdov <milosvova@gmail.com>
2016-03-20 22:51:34 +0300

    First program is done

---
+++ asm/task1.asm
@@ -0,0 +1,31 @@
+    ldc 1
+    st 4
+    ldc 1
+    ldc 1
+    st 3
+    ldc 1
+loop:
+    add
+    st 2
+    ld 2
+    ld 3
+    add
+    st 3
+    ldc 1
+    ld 3
+    ldc 200000
+    cmp
+    sub
+    br break
+    st 1
+    ld 3
+    ld 4
+    add
+    st 4
+    ld 3
+    ld 2
+    jmp loop
+break:
+    st 1
+    ld 4
+    ret
